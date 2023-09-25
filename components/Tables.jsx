00f15fefeac7b453b45475a8c9fc6d25ddec4b05
Alexis-Vacilli <alexisvacilli1@gmail.com>
2023-09-25 18:21:13 +0200

    bug fix

--- components/Tables.jsx
+++ components/Tables.jsx
@@ -4,14 +4,14 @@ const Tables = () => {
       <table className="min-w-full text-sm text-left text-gray-500">
         <thead className="text-xs text-gray-900 uppercase bg-[#EDF0FE]">
           <tr>
-            <th scope="col" class="p-4">
+            <th scope="col" className="p-4">
               <div className="flex items-center">
                 <input
                   id="checkbox-all-search"
                   type="checkbox"
                   className="w-4 h-4 text-blue-600 bg-gray-100 border-[#EDF0FE] rounded focus:ring-blue-500"
                 />
-                <label for="checkbox-all-search" className="sr-only">
+                <label  className="sr-only">
                   checkbox
                 </label>
               </div>
@@ -48,7 +48,7 @@ const Tables = () => {
                   type="checkbox"
                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
                 />
-                <label for="checkbox-table-search-1" className="sr-only">
+                <label className="sr-only">
                   checkbox
                 </label>
               </div>
@@ -72,7 +72,7 @@ const Tables = () => {
                   type="checkbox"
                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
                 />
-                <label for="checkbox-table-search-1" className="sr-only">
+                <label  className="sr-only">
                   checkbox
                 </label>
               </div>
